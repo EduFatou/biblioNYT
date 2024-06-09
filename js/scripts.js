a848f6b29cbe403e5c584684b2419e6e1f6991b4
EduFatou <edufatou@gmail.com>
2024-06-09 23:36:02 +0200

    filters working, style updated, css still unfinished

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//*********************variables*******************************
+//*********************variables************************************************************************
 
 const section = document.querySelector('#section');
 const divList = document.querySelector('#divList');
@@ -6,42 +6,104 @@ const fragment = document.createDocumentFragment();
 const loader = document.getElementById('loader');
 const header = document.getElementById('header');
 const main = document.querySelector('#main');
-const divFilter = document.querySelector('#filter-container');
-const filterInput = document.querySelector('#filterInput');
-const ShowOnly = document.querySelector('#showOnly')
-let dataList = document.querySelector('#datalist');
-let backButton;
+//los divs los capturo para poder borrar o mostrar los filtros
+const divCategoriesFilter = document.querySelector('#filter-container');
+const filterCategoriesInput = document.querySelector('#filterCategoriesInput');
+const divBooksFilter = document.querySelector('#filterBooks-container');
+const filterBooksInput = document.querySelector('#filterBooksInput');
+const ShowOnly = document.querySelector('#showOnly');
+const oldest = document.querySelector('#oldest');
+const newest = document.querySelector('#newest');
+const show5 = document.querySelector('#show5');
+//este es el unico botón a capturar para mostrarlo o borrarlo, los eventos capturan el resto de botones por su id
+const backButton = document.querySelector('#backButton');
+let datalist = document.querySelector('#datalist');
+let datalist2 = document.querySelector('#datalist2');
+//hay que declararlas en global para asignarle los valores en la llamada a la api
+//y que los filtros recojan esos valores y así no salga el portátil volando por la ventana
 let lists = [];
 let filteredLists = [];
-let arraySuggestions = [];
+let booklist = [];
+let listName = '';
+let arrayCategoriesSuggestions = [];
+let arrayBookSuggestions = [];
+let currentPage = 0;
+let pages = [];
+//botones de páginación
+const nextButton = document.createElement('button');
+const previousButton = document.createElement('button');
+nextButton.innerHTML = 'Next page';
+previousButton.innerHTML = 'Previous page';
+nextButton.setAttribute('id', 'next');
+previousButton.setAttribute('id', 'previous');
 
-//*********************events*******************************
 
-//buttons
+//*********************events************************************************************************
+
+
+//all buttons
+
 document.addEventListener('click', (evento) => {
+
+    //main
+
     if (evento.target.matches('.listButton')) {
         const category = evento.target.id;
         getBooklist(category);
-        filterInput.value = '';
+        filterCategoriesInput.value = '';
     }
-    if (evento.target.matches('.backButton')) {
-        clean(divList);
-        backButton.remove();
+    if (evento.target.matches('#backButton')) {
+        clear(divList);
+        divCategoriesFilter.style.display = 'block';
+        divBooksFilter.style.display = 'none';
+        backButton.style.display = 'none';
+        nextButton.style.display = 'none';
+        previousButton.style.display = 'none';
+
         getCategories();
-        filterInput.value = '';
+        filterCategoriesInput.value = '';
     }
-    if (evento.target.matches('#filterButton')) {
+    if (evento.target.matches('#filterButton'))
         searchByCategory();
-        filterInput.value = '';
-    }
+        
     if (evento.target.matches('#sortByAZ'))
         sortByAlphabet('AZ');
 
     if (evento.target.matches('#sortByZA'))
         sortByAlphabet('ZA');
+
+    //booklists
+
+    if (evento.target.matches('#filterBooksButton'))
+        searchBooks();
+
+    if (evento.target.matches('#sortBooksByAZ'))
+        sortBooksByAlphabet('AZ');
+
+    if (evento.target.matches('#sortBooksByZA'))
+        sortBooksByAlphabet('ZA');
 });
 
+//---------------------index events---------------------------------
+
+//input suggestions, search by category
+
+filterCategoriesInput.onkeyup = (ev) => {
+    let input = ev.target.value;
+    let filteredSuggestions = [];
+    if (input) filteredSuggestions = arrayCategoriesSuggestions.filter(data => {
+        return data.toLocaleLowerCase().startsWith(input.toLocaleLowerCase());
+    });
+    datalist.innerHTML = '';
+    filteredSuggestions.forEach((input) => {
+        const option = document.createElement('option');
+        option.value = input;
+        datalist.append(option);
+    });
+};
+
 //select, show by week or month
+
 ShowOnly.addEventListener('change', (evento) => {
     if (evento.target.value === 'WEEKLY' || evento.target.value === 'MONTHLY')
         showByPeriod(evento.target.value);
@@ -49,43 +111,96 @@ ShowOnly.addEventListener('change', (evento) => {
         getCategories();
 });
 
-//input, search by category
-filterInput.onkeyup = (ev) => {
+//select, sort by published date
+
+oldest.addEventListener('change', (evento) => {
+    if (evento.target.value === 'ascending' || evento.target.value === 'descending')
+        sortByOldDate(evento.target.value);
+    if (evento.target.value === 'everything')
+        getCategories();
+});
+
+newest.addEventListener('change', (evento) => {
+    if (evento.target.value === 'ascending' || evento.target.value === 'descending')
+        sortByNewDate(evento.target.value);
+    if (evento.target.value === 'everything')
+        getCategories();
+});
+
+
+//---------------------Booklists events---------------------------------
+
+//input suggestions, search by title or author
+
+filterBooksInput.onkeyup = (ev) => {
     let input = ev.target.value;
     let filteredSuggestions = [];
-    if (input) filteredSuggestions = arraySuggestions.filter(data => {
+    if (input) filteredSuggestions = arrayBookSuggestions.filter(data => {
         return data.toLocaleLowerCase().startsWith(input.toLocaleLowerCase());
     });
-    dataList.innerHTML = '';
+    datalist2.innerHTML = '';
     filteredSuggestions.forEach((input) => {
         const option = document.createElement('option');
         option.value = input;
-        dataList.append(option);
+        datalist2.append(option);
     });
 };
 
-//*********************functions*******************************
+//show five pages, append buttons
 
-const clean = elemento => elemento.innerHTML = '';
+show5.addEventListener('change', (evento) => {
+    if (evento.target.checked) {
+        for (let i = 0; i < booklist.length; i += 5) {
+            pages.push(booklist.slice(i, i + 5));
+        }
+        header.append(nextButton, previousButton);
+        printBooklist(pages[currentPage], listName);
+    } else {
+        printBooklist(booklist, listName);
+    }
+});
 
-//filters
+nextButton.addEventListener('click', () => {
+    if (currentPage < pages.length - 1) {
+        currentPage++;
+        printBooklist(pages[currentPage], listName);
+    }
+});
+
+previousButton.addEventListener('click', () => {
+    if (currentPage > 0) {
+        currentPage--;
+        printBooklist(pages[currentPage], listName);
+    }
+});
+
+
+//*********************filter functions************************************************************************
+
+const clear = elemento => elemento.innerHTML = '';
+
+//---------------------index filters---------------------------------
+
 const searchByCategory = () => {
-    const filterValue = filterInput.value.toLowerCase();
-    filteredLists = lists.filter(lista => lista.display_name.toLowerCase().includes(filterValue));
-    clean(divList);
+    backButton.style.display = 'flex';
+    const filterCategoriesValue = filterCategoriesInput.value.toLowerCase();
+    filteredLists = lists.filter(lista => lista.display_name.toLowerCase().includes(filterCategoriesValue));
+    clear(divList);
     printCategories(filteredLists);
 };
 
 const showByPeriod = (period) => {
-    periodfilteredLists = filteredLists.filter(lista => lista.updated === period);
-    clean(divList);
+    backButton.style.display = 'flex';
+    const periodfilteredLists = filteredLists.filter(lista => lista.updated === period);
+    clear(divList);
     printCategories(periodfilteredLists);
 };
 
 const sortByAlphabet = (order) => {
-    clean(divList);
+    backButton.style.display = 'flex';
+    clear(divList);
     const sortedList = filteredLists.sort((a, b) => {
-        if(order === 'AZ'){
+        if (order === 'AZ') {
             return a.display_name.localeCompare(b.display_name);
         } else if (order === 'ZA') {
             return b.display_name.localeCompare(a.display_name);
@@ -94,18 +209,80 @@ const sortByAlphabet = (order) => {
     printCategories(sortedList);
 };
 
+const sortByOldDate = (oldest) => {
+    backButton.style.display = 'flex';
+    clear(divList);
+    const sortedByOldestDate = filteredLists.sort((a, b) => {
+        if (oldest === 'ascending') {
+            return a.oldest_published_date.localeCompare(b.oldest_published_date);
+        } else if (oldest === 'descending') {
+            return b.oldest_published_date.localeCompare(a.oldest_published_date);
+        }
+    });
+    printCategories(sortedByOldestDate);
+};
 
-//index
+const sortByNewDate = (newest) => {
+    backButton.style.display = 'flex';
+    clear(divList);
+    const sortedByNewestDate = filteredLists.sort((a, b) => {
+        if (newest === 'ascending') {
+            return a.newest_published_date.localeCompare(b.newest_published_date);
+        } else if (newest === 'descending') {
+            return b.newest_published_date.localeCompare(a.newest_published_date);
+        }
+    });
+    printCategories(sortedByNewestDate);
+};
+
+
+//---------------------booklist filters-------------------------------------
+
+const searchBooks = () => {
+    const filterBooksValue = filterBooksInput.value.toLowerCase();
+    const filteredBooklists = booklist.filter(lista => lista.title.toLowerCase().includes(filterBooksValue) || lista.author.toLowerCase().includes(filterBooksValue));
+    console.log(filteredBooklists)
+    clear(divList);
+    printBooklist(filteredBooklists, listName);
+    
+};
+
+const sortBooksByAlphabet = (order) => {
+    clear(divList);
+    const sortedList = booklist.sort((a, b) => {
+        if (order === 'AZ') {
+            return a.title.localeCompare(b.title);
+        } else if (order === 'ZA') {
+            return b.title.localeCompare(a.title);
+        }
+    });
+    printBooklist(sortedList, listName);
+};
+
+// const showFiveBooks = () => {
+//     clear(divList);
+//     const fiveBooks = booklist.slice(0, 5);
+//     printBooklist(fiveBooks, listName);
+// };
+
+//*********************main functions************************************************************************
+
+//---------------------index functions----------------------------------------
+
 const getCategories = async () => {
+    header.style.display = 'none';
+    divBooksFilter.style.display = 'none';
+    backButton.style.display = 'none';
     loader.style.display = 'flex';
     const url = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=fIJzQt26lm7XPd0UdB5iJlFzxefOembt';
     document.getElementById('title').innerHTML = 'Categories';
     try {
         const respuesta = await fetch(url);
         if (respuesta.ok) {
+            header.style.display = 'block';
             const data = await respuesta.json();
             lists = data.results;
-            filteredLists = [...lists];
+            filteredLists = [...lists]; //para clonar lists sin alterarla, no estoy seguro de que sea necesario, pero dont try to fix it
             printCategories(lists);
             loader.style.display = 'none';
             return lists;
@@ -120,7 +297,7 @@ const getCategories = async () => {
 const printCategories = (lists) => {
 
     lists.forEach((list) => {
-        arraySuggestions.push(list.display_name);
+        arrayCategoriesSuggestions.push(list.display_name);
     });
 
     lists.forEach((elemento) => {
@@ -144,8 +321,12 @@ const printCategories = (lists) => {
     divList.append(fragment);
 };
 
-//funcion de lista de libros
+//---------------------booklist functions----------------------------------------
+
 const getBooklist = async (category) => {
+    divBooksFilter.style.display = 'block';
+    backButton.style.display = 'flex';
+    divCategoriesFilter.style.display = 'none';
     loader.style.display = 'flex';
     const url = `https://api.nytimes.com/svc/books/v3/lists/current/${category}.json?api-key=fIJzQt26lm7XPd0UdB5iJlFzxefOembt`;
     try {
@@ -153,8 +334,8 @@ const getBooklist = async (category) => {
         if (respuesta.ok) {
             divList.innerHTML = '';
             const data = await respuesta.json();
-            const listName = data.results;
-            const booklist = data.results.books;
+            listName = data.results.display_name;
+            booklist = data.results.books; // parece que no es necesario un filteredbooklist, embolia
             printBooklist(booklist, listName);
             loader.style.display = 'none';
             return booklist;
@@ -169,11 +350,11 @@ const getBooklist = async (category) => {
 
 const printBooklist = (booklist, listName) => {
     divList.innerHTML = '';
-    backButton = document.createElement('button');
-    backButton.innerHTML = '< Back to Index';
-    backButton.classList.add('backButton');
-    header.append(backButton);
-    document.getElementById('title').innerHTML = listName.display_name;
+    document.getElementById('title').innerHTML = listName;
+
+    booklist.forEach((book) => {
+        arrayBookSuggestions.push(book.title, book.author);
+    });
 
     booklist.forEach((libro) => {
         const containerLista = document.createElement('article');
@@ -197,14 +378,14 @@ const printBooklist = (booklist, listName) => {
     divList.append(fragment);
 };
 
-lists = getCategories()
+lists = getCategories() //respuesta asignada a lists para pintar las sugerencias
     .then(respuesta => {
         console.log(respuesta);
         return respuesta;
     })
     .catch(error => console.error(error))
 
-console.log(lists);
+// si lo descomento da error en consola porque está invocando cuando aun no tiene valores, me lo pongo de sombrero
 // getBooklist()
 //     .then(respuesta => console.log(respuesta))
 //     .catch(error => console.error(error))
